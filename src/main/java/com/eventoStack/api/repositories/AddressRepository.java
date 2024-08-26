282c83b8cad6a416473cb2ffac67a5ef9945b171
Victor Emanuel <victor.emanuel@sgnsistemas.com.br>
2024-08-26 18:19:36 -0300

    create repository

---
+++ src/main/java/com/eventoStack/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.eventoStack.api.repositories;
+
+import com.eventoStack.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/eventoStack/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.eventoStack.api.repositories;
+
+import com.eventoStack.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
